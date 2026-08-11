17063c8cce4c5d51d7735a6b373d561d05f9ef83
Djoystick <j.j.niccals2@gmail.com>
2026-08-11 03:28:09 +0300

    chore: remove temp debug scripts from repo

--- fix_ip.js
+++
@@ -1,6 +0,0 @@
-const fs = require('fs');
-let c = fs.readFileSync('api/index.js', 'utf8');
-const search = "req.ip?.replace(/:\\d+[^:]*$/, '')";
-c = c.split(search).join("req.ip");
-fs.writeFileSync('api/index.js', c);
-console.log("Fixed.");
